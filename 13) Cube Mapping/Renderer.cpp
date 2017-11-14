31cec29b6ddccc92f4bb7a41d7b092aefb2c32a7
b4031639 <b4031639@TERRAN01>
2017-11-14 14:35:37 +0000

    tut 14

--- 13) Cube Mapping/Renderer.cpp
+++ 13) Cube Mapping/Renderer.cpp
@@ -16,7 +16,7 @@ Renderer::Renderer(Window &parent) :OGLRenderer(parent) {
 		return;
 	}
 
-	quad->SetTexture(SOIL_load_OGL_texture(TEXTUREDIR"brick.tga", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
+	quad->SetTexture(SOIL_load_OGL_texture(TEXTUREDIR"water.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 	heightMap->SetTexture(SOIL_load_OGL_texture(TEXTUREDIR"Barren Reds.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 	heightMap->SetBumpMap(SOIL_load_OGL_texture(TEXTUREDIR"Barren RedsDOT3.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 
@@ -65,7 +65,7 @@ void Renderer::UpdateScene(float msec) {
 }void Renderer::RenderScene() {
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-	/*DrawSkybox();*/
+	DrawSkybox();
 	DrawHeightmap();
 	DrawWater();
 
@@ -96,37 +96,5 @@ void Renderer::DrawSkybox() {
 
 	glUseProgram(0);
 }
-void Renderer::DrawWater() {
-	SetCurrentShader(reflectShader);
-	SetShaderLight(*light);
-	glUniform3fv(glGetUniformLocation(currentShader -> GetProgram(),
-		"cameraPos"), 1, (float *)& camera -> GetPosition());
-
-	glUniform1i(glGetUniformLocation(currentShader -> GetProgram(),
-		"diffuseTex"), 0);
-
-	glUniform1i(glGetUniformLocation(currentShader -> GetProgram(),
-		"cubeTex"), 2);
-
-	glActiveTexture(GL_TEXTURE2);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMap);
-
-	float heightX = (RAW_WIDTH * HEIGHTMAP_X / 2.0f);
-
-	float heightY = 256 * HEIGHTMAP_Y / 3.0f;
-
-	float heightZ = (RAW_HEIGHT * HEIGHTMAP_Z / 2.0f);
-
-	modelMatrix =
-		Matrix4::Translation(Vector3(heightX, heightY, heightZ)) *
-		Matrix4::Scale(Vector3(heightX, 1, heightZ)) *
-		Matrix4::Rotation(90, Vector3(1.0f, 0.0f, 0.0f));
-
-	textureMatrix = Matrix4::Scale(Vector3(10.0f, 10.0f, 10.0f)) *
-		Matrix4::Rotation(waterRotate, Vector3(0.0f, 0.0f, 1.0f));
-	UpdateShaderMatrices();
-
-	quad -> Draw();
-
-	glUseProgram(0);
-}
+void Renderer::DrawWater() {
+	SetCurrentShader(reflectShader);	SetShaderLight(*light);	glUniform3fv(glGetUniformLocation(currentShader->GetProgram(), "cameraPos"), 1, (float*)&camera->GetPosition());	glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "diffuseTex"), 0);	glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "cubeTex"), 2);	glActiveTexture(GL_TEXTURE2);	glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMap);	float heightX = (RAW_WIDTH*HEIGHTMAP_X / 2.0f);	float heightY = 256 * HEIGHTMAP_Y / 3.0f;	float heightZ = (RAW_HEIGHT*HEIGHTMAP_Z / 2.0f);	modelMatrix = Matrix4::Translation(Vector3(heightX, heightY, heightZ))* Matrix4::Scale(Vector3(heightX, 1, heightZ))*		Matrix4::Rotation(90, Vector3(1.0f, 0.0f, 0.0f));	textureMatrix = Matrix4::Scale(Vector3(10.0f, 10.0f, 10.0f)) * Matrix4::Rotation(waterRotate, Vector3(0.0f, 0.0f, 1.0f));	UpdateShaderMatrices();	quad->Draw();		glUseProgram(0);}
