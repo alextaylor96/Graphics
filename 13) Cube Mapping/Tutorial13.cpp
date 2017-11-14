e1a0d40394b8fe50dae7bd77b0ab5cc9988bee8a
b4031639 <b4031639@TERRAN01>
2017-11-14 16:06:25 +0000

    .

--- 13) Cube Mapping/Tutorial13.cpp
+++ 13) Cube Mapping/Tutorial13.cpp
@@ -4,20 +4,20 @@
 #include "Renderer.h"
 
 int main() {
-	Window w("Cube Mapping! sky textures courtesy of http://www.hazelwhorley.com", 800,600,false);
-	if(!w.HasInitialised()) {
+	Window w("Cube Mapping! sky textures courtesy of http://www.hazelwhorley.com", 800, 600, false);
+	if (!w.HasInitialised()) {
 		return -1;
 	}
-	
+
 	Renderer renderer(w);
-	if(!renderer.HasInitialised()) {
+	if (!renderer.HasInitialised()) {
 		return -1;
 	}
 
 	w.LockMouseToWindow(false);
 	w.ShowOSPointer(true);
 
-	while(w.UpdateWindow() && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE)){
+	while (w.UpdateWindow() && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE)) {
 		renderer.UpdateScene(w.GetTimer()->GetTimedMS());
 		renderer.RenderScene();
 	}
